8782d71c07b8836d5d79a04c48279ecaa38ea256
stefan-jo <stefanjosef0@gmail.com>
2021-01-23 10:44:42 +0100

    truncation added

--- flask/app/classes/sentence_embedder.py
+++ flask/app/classes/sentence_embedder.py
@@ -2,30 +2,29 @@ from transformers import AutoTokenizer, AutoModel
 import torch
 
 class SentenceEmbedder:
-    def __init__(self):
-        self.tokenizer = AutoTokenizer.from_pretrained("DeepPavlov/bert-base-bg-cs-pl-ru-cased")
-        self.model = AutoModel.from_pretrained("DeepPavlov/bert-base-bg-cs-pl-ru-cased")
+    def __init__(self, path="DeepPavlov/bert-base-bg-cs-pl-ru-cased"):
+        self.tokenizer = AutoTokenizer.from_pretrained(path)
+        self.model = AutoModel.from_pretrained(path)
 
-    def tokenize(self, sentence):
-        return self.tokenizer.encode(sentence)
+    def tokenize(self, sentence, max_length=512):
+        return self.tokenizer.encode(sentence, max_length=max_length, truncation=True)
 
-    def tokenize_batch(self, sentences, max_length=None):
-        tokens = self.tokenizer.batch_encode_plus(sentences, padding=True, max_length=max_length)
+    def tokenize_batch(self, sentences, max_length=512):
+        tokens = self.tokenizer.batch_encode_plus(sentences, padding=True, max_length=max_length, truncation=True)
         return tokens['input_ids']
 
-    def encode(self, sentence, pooled=False):
-        input_ids = torch.tensor(self.tokenize(sentence)).unsqueeze(0)
-        tensors_tuple = self.model(input_ids)
-        print(tensors_tuple)
+    def encode(self, sentence, pooled=False, max_length=512):
+        input_ids = torch.tensor(self.tokenize(sentence, max_length=max_length)).unsqueeze(0)
+        out = self.model(input_ids)
         if pooled:
-            return tensors_tuple[1].squeeze().detach().numpy()
+          return out['pooler_output'].squeeze().detach().numpy()
         else:
-            return tensors_tuple[0].mean(1).squeeze().detach().numpy()
+          return out['last_hidden_state'].mean(1).squeeze().detach().numpy()
 
-    def encode_many(self, sentences, pooled=False, max_length=None):
+    def encode_many(self, sentences, pooled=False, max_length=512):
         input_ids = torch.tensor(self.tokenize_batch(sentences, max_length=max_length))
         out = self.model(input_ids)
         if pooled:
-            return out['pooler_output'].squeeze().detach().numpy()
+          return out['pooler_output'].detach().numpy()
         else:
-            return out['last_hidden_state'].mean(1).squeeze().detach().numpy()
+          return out['last_hidden_state'].mean(1).detach().numpy()
